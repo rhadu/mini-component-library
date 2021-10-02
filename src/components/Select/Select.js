98b9ba2c418c52d38dac7e166cfc65a731f37c72
Radu Turtoi <radu.turtoi@gmail.com>
2021-10-02 22:50:26 +0300

    select styling

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from '../../constants'
+import Icon from '../Icon'
+import { getDisplayedValue } from './Select.helpers'
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  const displayedValue = getDisplayedValue(value, children)
+
+  // <SelectIcon id='chevron-down' size='24'></SelectIcon>
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': '24px' }}>
+          <Icon id='chevron-down' size='24' strokeWidth='2'></Icon>
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
+  )
+}
 
-export default Select;
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus-visible + & {
+    outline: 2px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  display: inline-block;
+  pointer-events: none;
+  width: var(--size);
+  height: var(--size);
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+`
+
+export default Select
